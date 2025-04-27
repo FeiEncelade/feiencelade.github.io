987c9ffe36b3062b778fa47966a2325df0f71743
Fei_ENCELADE <opheliajian@gmail.com>
2025-04-28 00:43:18 +0200

    maj20250428V3

---
+++ src/settings.f979c.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/00-home/home.fire",orientation:"",jsList:["assets/public/js/data/data.f978c.js"],bundleVers:{internal:"1b082",resources:"3e31c",main:"39573"}};
